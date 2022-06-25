f856b0a650e3e80cdedcb40c86560f9476e7b091
OverlineJunior <franciscosebastianyjunior@hotmail.com>
2022-06-24 21:41:19 -0300

    add most stuff.

--- src/init.lua
+++ src/init.lua
@@ -1,1 +1,88 @@
-return {}
+export type Data = {[any]: any}
+type ModifierFn = (number, Data) -> number?
+type Modifiers = {[Humanoid]: {[number]: ModifierFn?}}
+type GlobalModifiers = {[number]: ModifierFn?}
+
+local modifiers: Modifiers = {}
+local globalModifiers: GlobalModifiers = {}
+local nextID = 1
+
+local Hearts = {}
+
+
+function Hearts.Damage(target: Humanoid, amount: number, data: Data?)
+    if amount < 0 then return end
+
+    Hearts._Add(target, -amount, data)
+end
+
+
+function Hearts.Heal(target: Humanoid, amount: number, data: Data?)
+    if amount < 0 then return end
+
+    Hearts._Add(target, amount, data)
+end
+
+
+function Hearts.AddModifier(target: Humanoid, modifierFn: ModifierFn): number
+    if not modifiers[target] then
+        modifiers[target] = {}
+    end
+
+    modifiers[target][nextID] = modifierFn
+    nextID += 1
+
+    return nextID - 1
+end
+
+
+function Hearts.AddGlobalModifier(modifierFn: ModifierFn): number
+    globalModifiers[nextID] = modifierFn
+    nextID += 1
+
+    return nextID - 1
+end
+
+
+function Hearts.RemoveModifier(id: number)
+    globalModifiers[id] = nil
+
+    for _, dict in modifiers do
+        dict[id] = nil
+    end
+end
+
+
+function Hearts._Add(target: Humanoid, healthSum: number, data: Data?)
+    data = data or {}
+
+    local function applyModifier(modifierFn: ModifierFn)
+        local newHealthSum = modifierFn(healthSum, data)
+
+        assert(
+            type(newHealthSum) == 'number' or newHealthSum == nil,
+            string.format('Expected middleware function to return either a number or nil, got %q instead', typeof(newHealthSum))
+        )
+
+        healthSum = newHealthSum or healthSum
+    end
+
+    for _, modifierFn in modifiers[target] or {} do
+        applyModifier(modifierFn)
+    end
+
+    for _, modifierFn in globalModifiers do
+        applyModifier(modifierFn)
+    end
+
+    if healthSum == 0 then return end
+
+    if healthSum > 0 then
+        target.Health += healthSum
+    else
+        target:TakeDamage(-healthSum)
+    end
+end
+
+
+return Hearts
--- tests/Test.server.lua
+++ tests/Test.server.lua
@@ -1,3 +1,24 @@
 local Hearts = require(script.Parent.Hearts)
 
-print(Hearts.Key)
+local me = workspace.Me.Humanoid
+local enemy = workspace.Enemy.Humanoid
+
+local id1 = Hearts.AddModifier(me, function(healthSum: number)
+    if healthSum < 0 then
+        return healthSum / 2
+    end
+end)
+
+local id2 = Hearts.AddGlobalModifier(function(healthSum: number)
+    if healthSum < 0 then
+        return healthSum / 2
+    end
+end)
+
+Hearts.RemoveModifier(id1)
+Hearts.RemoveModifier(id2)
+
+Hearts.Damage(me, 50)
+Hearts.Damage(enemy, 50)
+task.wait(2)
+Hearts.Heal(me, 50)
